53b1ab5089b2fe1562d033313a8b97432254168d
Alexander Perepelitsyn <alexander.p@randrmusic.com>
2013-06-07 23:41:41 +0300

    Initial Commit

---
+++ EPDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EPDemo
+//
+//  Created by Alexander Perepelitsyn on 6/7/13.
+//  Copyright (c) 2013 Alexander Perepelitsyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ EPDemoTests/EPDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  EPDemoTests.h
+//  EPDemoTests
+//
+//  Created by Alexander Perepelitsyn on 6/7/13.
+//  Copyright (c) 2013 Alexander Perepelitsyn. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EPDemoTests : SenTestCase
+
+@end
